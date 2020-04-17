aff04fdff39899384d3a9203616ef51a929cc8a8
ben <1294353939@qq.com>
2020-04-17 15:29:34 +0800

    修改文件路径

---
+++ geekTimeSwiftUI/Model/Product.swift
@@ -0,0 +1,22 @@
+//
+//  Product.swift
+//  geekTimeSwiftUI
+//
+//  Created by Ben Lv on 2020/1/25.
+//  Copyright © 2020 ruibo. All rights reserved.
+//
+
+import Foundation
+struct Product {
+    
+       var imageUrl : String
+       var name : String
+       var desc : String
+       var price : Int
+       var teacher : String
+       var total : Int
+       var update : Int
+       var studentCount :Int
+       var detail : String
+       var courseList : String
+}
